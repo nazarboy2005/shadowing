5d3d0aa2bb8395245aab836592036b30282dbfe5
nazarboy <rakhmonovnazarboy@gmail.com>
2024-10-03 00:06:50 +0300

    some changes to templates login, register, verify email

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import AccountModel, ConfirmationCodesModel
 
-# Register your models here.
+@admin.register(AccountModel)
+class AccountModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'country')
+    search_fields = ('user__username', 'full_name', 'country')
+    list_filter = ('country',)
+    readonly_fields = ('user',)
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'full_name', 'country')
+        }),
+    )
+
+@admin.register(ConfirmationCodesModel)
+class ConfirmationCodesModelAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'created_at')
+    search_fields = ('code', 'user__username')
+    readonly_fields = ('code', 'user', 'created_at')
+    fieldsets = (
+        (None, {
+            'fields': ('code', 'user', 'created_at')
+        }),
+    )
